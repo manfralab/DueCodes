e54f6dbce0cda1270863c6a71756e7602ab8e223
Nik Hartman <nik.hartman@gmail.com>
2019-07-08 12:27:18 -0400

    trying to add config

---
+++ sqpurdue/station_configurations/load.py
@@ -0,0 +1,12 @@
+''' helpers to load configuration without dealing with paths in notebook '''
+
+import pathlib
+
+yaml_lookup = {
+    'BlueFors': 'BFXLD1.yaml',
+    '4K magnet': 'oxford_4K_magnet_station.yaml',
+}
+
+def config_path(system_name):
+    ''' load config path by system name '''
+    pass
