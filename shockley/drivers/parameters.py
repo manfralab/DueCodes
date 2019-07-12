07a0854e666b35378d4557d7de24215eab91f81d
Nik Hartman <nik.hartman@gmail.com>
2019-07-12 18:38:31 -0400

    working on device module

--- shockley/drivers/parameters.py
+++ shockley/drivers/parameters.py
@@ -4,7 +4,7 @@ import qcodes as qc
 class CounterParam(qc.Parameter):
     def __init__(self, name):
         # only name is required
-        super().__init__(name, label='Times this has been read',
+        super().__init__(name, label='counter',
                          unit='int',
                          vals=qc.validators.Ints(min_value=0),
                          docstring='counts how many times get has been called '
