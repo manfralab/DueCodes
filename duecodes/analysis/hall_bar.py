de6ed3423972d0aa48d70d71522aa4f94c9b0447
nikhartman <nik.hartman@gmail.com>
2021-03-29 22:21:09 -0400

    hall updates

--- duecodes/analysis/hall_bar.py
+++ duecodes/analysis/hall_bar.py
@@ -3,6 +3,7 @@ import numpy as np
 import xarray as xr
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
 from qcodes.dataset.data_set import DataSet
 from duecodes.analysis.load import get_dataset_by_identifier
 
@@ -22,8 +23,7 @@ def _get_field_gate_params(ds: DataSet) -> Tuple[str, str]:
         raise KeyError('Could not find magnetic field or gate voltage parameter.')
     return field_param, gate_param
 
-
-def calculate_density_mobility(
+def clean_hall_data(
     identifier: Union[int, str],
     vxx_param_name: str, vxy_param_name: str,
     bias_current: float, width: float, length: float,
@@ -36,11 +36,20 @@ def calculate_density_mobility(
     arr = ds.to_xarray_dataset()
     arr = arr.rename({gate_param: 'gate', field_param: 'field'})
 
-    vxx = arr[vxx_param_name].real/voltage_amp_xx
-    vxy = arr[vxy_param_name].real/voltage_amp_xy
 
-    rxx = (vxx/bias_current).rename('R_xx')/(length/width) # R per square
-    rxy = (vxy/bias_current).rename('R_xy')
+    arr[vxx_param_name] = arr[vxx_param_name].real/voltage_amp_xx # vxx
+    arr[vxy_param_name] = arr[vxy_param_name].real/voltage_amp_xy # vxy
+
+    arr[vxx_param_name] = \
+        (arr[vxx_param_name]/bias_current)/(length/width) # rxx, Ohms per square
+    arr[vxy_param_name] = (arr[vxy_param_name]/bias_current) # rxy, Ohms
+
+    return arr.rename({vxx_param_name: 'R_xx', vxy_param_name: 'R_xy'})
+
+def calculate_density_mobility(data: xr.Dataset) -> xr.Dataset:
+
+    rxx = data['R_xx']
+    rxy = data['R_xy']
 
     popt = rxy.polyfit('field', 1).drop('degree')
 
@@ -55,14 +64,26 @@ def calculate_density_mobility(
     return results
 
 def plot_density_mobility(
-    arr: xr.Dataset
+    results: xr.Dataset
     ) -> Tuple[matplotlib.axes.Axes, matplotlib.colorbar.Colorbar]:
 
     fig, ax = plt.subplots(1,1)
 
-    im = ax.scatter(arr['density'], arr['mobility'], c=arr['gate'])
+    im = ax.scatter(results['density'], results['mobility'],
+        c=results['gate'])
     cb = fig.colorbar(im, ax=ax)
     cb.set_label('V_gate (V)')
+
+    v_gate_at_max = results['mobility'].idxmax()
+    u_max = results['mobility'].loc[v_gate_at_max].values
+    n_at_max = results['density'].loc[v_gate_at_max].values
+    print(f'{u_max:.2e}, {n_at_max:.2e}')
+
+    at = AnchoredText(f'{u_max:.2e} cm^2/(Vs)\nat {n_at_max:.2e} 1/cm^2',
+                      loc='upper right', prop=dict(size=10), frameon=True,
+                      )
+    at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+    ax.add_artist(at)
 
     ax.set_ylabel('mobility (cm^2/(Vs))')
     ax.set_xlabel('density (1/cm^2)')
